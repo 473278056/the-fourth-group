f8a8cf1f5796db55e3ca96a276570039c06a63ed
Aries <473278056@qq.com>
2018-11-02 14:19:30 +0800

    删除Log 模块

---
+++ SMS/src/main/java/com/suixingpay/SmsApplication.java
@@ -0,0 +1,13 @@
+package com.suixingpay;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.suixingpay.mapper")
+@SpringBootApplication
+public class SmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmsApplication.class, args);
+    }
+}
---
+++ SMS/src/main/java/com/suixingpay/mapper/StudentMapper.java
@@ -0,0 +1,22 @@
+package com.suixingpay.mapper;
+
+import com.suixingpay.pojo.Student;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentMapper {
+    List<Student> queryAllStudent();
+
+    Student queryStudentByid(@Param("id") Integer id);
+
+    //List<Student> queryUserByName(@Param("name") String name);
+
+    int updateStudentById(Student student);
+
+    int delStudentById(@Param("id") Integer id);
+
+    int insertStudent(Student student);
+}
